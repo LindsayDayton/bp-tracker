63190c902d3df8a9ebd849f28a2cf3c48290d9fa
LindsayDayton <llashell@gmail.com>
2025-02-06 16:55:23 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   output: 'standalone',
   images: {
     unoptimized: true
   }
 }
-
-module.exports = nextConfig
